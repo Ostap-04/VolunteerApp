34331101d6d54cabe18f9228abb82931c5081bc9
Vorobienko Bohdan <bogdanvorobienko@gmail.com>
2023-02-26 16:43:50 +0200

    vc-38 home page done

---
+++ VolunteerApp/ClientApp/src/app/loginBtn/loginBtn.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-button',
+  templateUrl: './loginBtn.component.html',
+  styleUrls: ['./loginBtn.component.css'],
+})
+export class LoginBtnComponent {}
---
+++ VolunteerApp/ClientApp/src/app/signupBtn/signupBtn.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-signup-button',
+  templateUrl: './signupBtn.component.html',
+  styleUrls: ['./signupBtn.component.css'],
+})
+export class SignUpBtnComponent {}
